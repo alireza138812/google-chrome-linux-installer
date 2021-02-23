885318a62fd51ee108ff5e7e4e9ca973d59af4ea
alireza138812 <77526328+alireza138812@users.noreply.github.com>
2021-02-23 17:28:24 +0330

    Add files via upload

---
+++ install-google-chrome-2.0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+echo "Hi installing your app(google-chrome)"
+#you can edit this file
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo "type y and press enter"
+#you can add other pk manager to install in other distro
+echo "arch base and debian base supported"
+sudo yaourt google-chrome
+sudo yay google-chrome
+sudo pamac build google-chrome
+#pkg name is "google-chrome"
+echo"oooooohhh"
+echo "install is have eror or not have eror and insalled"
